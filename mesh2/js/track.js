54a49dc0643104444ea00547addbe7a07aefe8ec
Nagwal <nagwalmail@gmail.com>
2016-02-18 14:40:15 +0300

    добавлены маркеры начала и конца маршрута, изменен маркер направления

--- mesh2/js/track.js
+++ mesh2/js/track.js
@@ -18,6 +18,37 @@ Track.prototype.createFromJson = function( json_dots ) {
 	var pts = [];
 	var odd = 0;
     var arrow_counter = 0;
+    var symbolStart = {
+        path: 'M -2,0 0,-2 2,0 0,2 z',
+        strokeColor: '#000000',
+        fillColor: '#808080',
+        fillOpacity: 1,
+        anchor: { x: 0, y: -1 }
+    };
+    
+    var symbolFinish = {
+        path: google.maps.SymbolPath.CIRCLE,
+        scale: 4,
+        strokeWeight: 2,
+        anchor: { x: 0, y: 1 },
+        strokeColor: "#404040"        
+    }
+
+    var symbolFinishSmall = {
+        path: google.maps.SymbolPath.CIRCLE,
+        scale: 2,
+        strokeWeight: 2,
+        anchor: { x: 0, y: 2 },
+        strokeColor: "#FFFFFF"        
+    }
+    
+    var symbolArrow = {
+        path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+        strokeOpacity: 0.8,
+        strokeColor: "#4285F4",
+        anchor: { x: -2, y: 0 },
+    }
+      
 	for( var dot in json_dots ) {
         var d = new google.maps.LatLng( json_dots[ dot ].lat, json_dots[ dot ].lng );
         if( this.first == null ) {
@@ -27,10 +58,36 @@ Track.prototype.createFromJson = function( json_dots ) {
         pts.push( d );
         var speed = Math.round(json_dots[ dot ].speed / 2);
       
+      
         if( odd > this.dots_in_path ) {
           odd = 0;
           arrow_counter++;
-          var path = new TrackPath( pts, speed, json_dots[ dot ], arrow_counter % 5 == 0 );
+          var icons = [ 
+            {
+                icon: arrow_counter >= ( json_dots.length / ( this.dots_in_path + 2 ) - 1 ) ?
+                    symbolFinish : null,
+                    
+                offset: "100%"
+            },
+            {
+                icon: arrow_counter >= ( json_dots.length / ( this.dots_in_path + 2 ) - 1 ) ?
+                    symbolFinishSmall : null,
+                    
+                offset: "100%"
+            },
+            {
+                icon: arrow_counter == 1 ? symbolStart : null,
+                offset: "0%"
+            },
+            {
+                icon: arrow_counter % this.dots_in_path == 0 ? 
+                    symbolArrow : null,
+                    
+                offset: "50%"
+            }
+         ];
+
+          var path = new TrackPath( pts, speed, json_dots[ dot ], icons );
           path.setTrack( this );
           pts = [];
           pts.push( d );
@@ -45,12 +102,9 @@ Track.prototype.addPath = function( path ) {
 	this.track_path.push( path );	
 }; 
 
-function TrackPath( points, speed, json_dot, with_arrow ) {
+function TrackPath( points, speed, json_dot, icons ) {
 	this.track = {}
 	var color = createColor( speed );	
-    var iconsetngs = {
-        path: google.maps.SymbolPath.FORWARD_OPEN_ARROW
-    };
 
 	this.polyline = new google.maps.Polyline({
 		path: points,
@@ -58,10 +112,7 @@ function TrackPath( points, speed, json_dot, with_arrow ) {
 		strokeColor: color,
 		strokeOpacity: 1.0,
 		strokeWeight: 2,
-        icons: [ {
-            icon: with_arrow ? iconsetngs : null,
-            offset: "100%"
-        } ]
+        icons: icons
 	});
 	
 	this.polyline.info = json_dot;
